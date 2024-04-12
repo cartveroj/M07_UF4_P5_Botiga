4b86f9f273553db9f767bfaa3ee4ec7064e92660
Gemma <gemma.garrigosa@gmail.com>
2024-04-12 17:02:01 +0200

    Modificats settings

--- botiga_grupG/botiga_grupG/settings.py
+++ botiga_grupG/botiga_grupG/settings.py
@@ -75,10 +75,15 @@ WSGI_APPLICATION = 'botiga_grupG.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'BOTIGA_DB',
+        'USER': 'gemma',
+        'PASSWORD': 'system',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
