fa6a7401b6ab6a2bba0b363d0211410aa589654f
Gemma <gemma.garrigosa@gmail.com>
2024-04-24 17:00:32 +0200

    Creat metode get d'un sol Producte

--- botiga_grupG/cataleg/urls.py
+++ botiga_grupG/cataleg/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('',views.hello_world, name='index'),
     path('crearProd/', views.afegir_producte, name='crearProducte'),
     path('updateProd/<str:pk>/', views.actualitza_producte, name='actualitzaProducte'),
-    path('productes/', views.veure_productes, name= 'productes')
+    path('productes/', views.veure_productes, name= 'productes'),
+    path('producte/<str:pk>/', views.veure_producte, name='producte')
 ]
