e40806b6d010211045c640ae36b18f3fd0c3bfc7
Gemma <gemma.garrigosa@gmail.com>
2024-05-03 16:11:37 +0200

    modificat serializer de producte perque no feia falta la id

--- botiga_grupG/cataleg/serializers.py
+++ botiga_grupG/cataleg/serializers.py
@@ -4,7 +4,7 @@ from rest_framework import serializers
 class CatalegSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Cataleg
-        fields = ['id','producte_id']
+        fields = ['producte_id']
 
 class ProductesSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
