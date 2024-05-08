a7ede3ef2eeee0683630f0026e1250b4ee4fd7ee
veronica <cartagenaveronica.19@gmail.com>
2024-05-08 18:50:11 +0200

    resolviendo conflictos

--- botiga_grupG/botiga_grupG/settings.py
+++ botiga_grupG/botiga_grupG/settings.py
@@ -38,17 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-<<<<<<< HEAD
-    'carreto.apps.CarretoConfig',
-    'cataleg.apps.CatalegConfig',
-    'pagaments.apps.PagamentsConfig',
-    'comandes.apps.ComandesConfig'
-=======
     'cataleg.apps.CatalegConfig',
     'carreto.apps.CarretoConfig',
     'comandes.apps.ComandesConfig',
     'pagaments.apps.PagamentsConfig',
->>>>>>> origin/pagaments
 ]
 
 MIDDLEWARE = [
@@ -86,7 +79,6 @@ WSGI_APPLICATION = 'botiga_grupG.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-<<<<<<< HEAD
      'default': {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "botiga_UF5",
@@ -94,15 +86,6 @@ DATABASES = {
         "PASSWORD": "pirineus",
         "HOST": "127.0.0.1",
         "PORT": "5432",
-=======
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'ITIC_DB',
-        'USER': 'gemma',
-        'PASSWORD': 'system',
-        'HOST': 'localhost',
-        'PORT': '5432',
->>>>>>> origin/pagaments
     }
 }
 
--- botiga_grupG/botiga_grupG/urls.py
+++ botiga_grupG/botiga_grupG/urls.py
@@ -15,14 +15,6 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-<<<<<<< HEAD
-from django.urls import include, path
-from rest_framework import routers
-
-router = routers.DefaultRouter()
-
-=======
->>>>>>> origin/pagaments
 from django.urls import path, include
 
 urlpatterns = [
